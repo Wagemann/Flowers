e65b36948dc158f01f73282840e40b2611e0e00b
Nick <wagemannnick@gmail.com>
2022-11-05 12:28:41 -0400

    setup server got data from endpoint

---
+++ Server/server.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.envPort || 4000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cors());
+
+let data = []
+
+function callExternalApi(){
+    const url = "https://jsonplaceholder.typicode.com/posts"
+    fetch(url)
+    .then((res)=>{res.json()
+    .then((backendData)=>{
+        data =backendData
+        })
+    })
+}
+
+app.get("/api/v1/data", async(req,res)=>{
+    res.send(data)
+})
+
+app.listen(4000, ()=>{
+    console.log(`Server is running on ${PORT}`)
+    callExternalApi()
+})
